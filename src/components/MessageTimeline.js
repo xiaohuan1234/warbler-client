09ae19795a5277366f920a75df7008156f6c9248
Xiaohuan Li <xiaohuan.li@gmail.com>
2019-02-01 17:47:08 +0000

    warbler client first commit with working login/signup

---
+++ src/components/MessageTimeline.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MessageList from "../containers/MessageList";
+// import UserAside from "./UserAside";
+
+const MessageTimeline = props => {
+  return (
+    <div className="row">
+      
+      <MessageList />
+    </div>
+  );
+};
+
+export default MessageTimeline;
